79b367f3cd47ee155b576f0da8d4796e254f72a9
Anna Samar <annsummmer@gmail.com>
2019-09-29 22:05:29 +0300

    add tests

--- robot.spec.js
+++ robot.spec.js
@@ -10,21 +10,33 @@ describe('Robot prototype', () => {
   it ('Should move in east direction when asked', () => {
     robot.clean(1, 0, 0, ['E', 10]);
     expect(robot.getCoords()).toEqual([10, 0]);
+    expect([...robot.uniqueCoords]).toEqual(
+      ['0,0', '1,0', '2,0', '3,0', '4,0', '5,0', '6,0', '7,0', '8,0', '9,0', '10,0']
+    );
   });
 
   it ('Should move in west direction when asked', () => {
     robot.clean(1, 10, 0, ['W', 10]);
     expect(robot.getCoords()).toEqual([0, 0]);
+    expect([...robot.uniqueCoords]).toEqual(
+      ['10,0', '9,0', '8,0', '7,0', '6,0', '5,0', '4,0', '3,0', '2,0', '1,0', '0,0']
+    );
   });
 
   it ('Should move in south direction when asked', () => {
     robot.clean(1, 0, 0, ['S', 10]);
     expect(robot.getCoords()).toEqual([0, 10]);
+    expect([...robot.uniqueCoords]).toEqual(
+      ['0,0', '0,1', '0,2', '0,3', '0,4', '0,5', '0,6', '0,7', '0,8', '0,9', '0,10']
+    );
   });
 
   it ('Should move in north direction when asked', () => {
     robot.clean(1, 0, 10, ['N', 10]);
     expect(robot.getCoords()).toEqual([0, 0]);
+    expect([...robot.uniqueCoords]).toEqual(
+      ['0,10', '0,9', '0,8', '0,7', '0,6', '0,5', '0,4', '0,3', '0,2', '0,1', '0,0']
+    );
   });
 
   it ('Should start from initial position', () => {
